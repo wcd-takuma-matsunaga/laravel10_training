dde89b2442535c9665aa7579682e1b0293cef682
wcd-takuma-matsunaga <taku-matsunaga@wearcoord.com>
2023-04-11 15:38:16 +0900

    create

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+// Postのリソースコントローラーを使用したルーティング
+Route::resource('post', PostController::class);
 
 Route::get('/', function () {
     return view('welcome');
